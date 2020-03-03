7086c27ec64c0421419b344243a8845d11610424
Lucas de Grande <lucasdegrande@gmail.com>
2020-03-03 20:36:46 -0300

    Adiciona possibilidade de copiar inicio do dia até hora atual ou dia atual, mas o dia inteiro
    Ambas as possibilidades para poder fazer pesquisas no banco de dados

--- Wox.Plugin.Date/Main.cs
+++ Wox.Plugin.Date/Main.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Windows.Forms;
 
 namespace Wox.Plugin.Date
 {
@@ -13,14 +14,42 @@ namespace Wox.Plugin.Date
         {
             var results = new List<Result>();
 
+            var dataHoraAtual = DateTime.Now;
             results.Add(new Result()
             {
-                Title = $"{DateTime.Now}",
-                SubTitle = $"Data/Hora Atual",
+                Title = $"{dataHoraAtual}",
+                SubTitle = $"Data/Hora Atual - Enviar para o clipboard",
                 IcoPath = "Images\\icon.png",
                 Action = e =>
                 {
-                    return false;
+                    Clipboard.SetText($"{dataHoraAtual}");
+                    return true;
+                }
+            });
+
+            var dataDoInicioDoDiaAteHoraAtual = $@"BETWEEN ""{dataHoraAtual.ToString("yyyy-MM-dd")} 00:00:00"" AND ""{dataHoraAtual.ToString("yyyy-MM-dd HH:mm:ss")}""";
+            results.Add(new Result()
+            {
+                Title = $"Inicio do dia atual até a hora atual para pesquisas no banco",
+                SubTitle = $"{dataDoInicioDoDiaAteHoraAtual}",
+                IcoPath = "Images\\icon.png",
+                Action = e =>
+                {
+                    Clipboard.SetText(dataDoInicioDoDiaAteHoraAtual);
+                    return true;
+                }
+            });
+
+            var diaCompletoParaPesquisaNoBanco = $@"BETWEEN ""{dataHoraAtual.ToString("yyyy-MM-dd")} 00:00:00"" AND ""{dataHoraAtual.ToString("yyyy-MM-dd")} 23:59:59""";
+            results.Add(new Result()
+            {
+                Title = $"Dia completo atual para pesquisas no banco",
+                SubTitle = $"{diaCompletoParaPesquisaNoBanco}",
+                IcoPath = "Images\\icon.png",
+                Action = e =>
+                {
+                    Clipboard.SetText(diaCompletoParaPesquisaNoBanco);
+                    return true;
                 }
             });
 
