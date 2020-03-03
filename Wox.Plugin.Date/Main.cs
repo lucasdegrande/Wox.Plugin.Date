d38b541df6f60c61721d3553196e93616bc12f1f
Lucas de Grande <lucasdegrande@gmail.com>
2020-03-03 19:20:16 -0300

    Adiciona o código gerado

---
+++ Wox.Plugin.Date/Main.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Wox.Plugin.Date
+{
+    public class Main : IPlugin
+    {
+        public void Init(PluginInitContext context)
+        {
+        }
+
+        public List<Result> Query(Query query)
+        {
+            var results = new List<Result>();
+
+            results.Add(new Result()
+            {
+                Title = $"{DateTime.Now}",
+                SubTitle = $"Data/Hora Atual",
+                IcoPath = "Images\\icon.png",
+                Action = e =>
+                {
+                    return false;
+                }
+            });
+
+            return results;
+        }
+
+    }
+}
