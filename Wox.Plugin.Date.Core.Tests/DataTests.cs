9362bc61b057620975012b017c4337e79af48f9f
Lucas de Grande <lucasdegrande@gmail.com>
2020-03-15 17:25:51 -0300

    Adiciona testes adicionais para validação da data gerada

--- Wox.Plugin.Date.Core.Tests/DataTests.cs
+++ Wox.Plugin.Date.Core.Tests/DataTests.cs
@@ -17,8 +17,27 @@ namespace Wox.Plugin.Date.Core.Tests
         public void Obter_OntemInformado_DeveRetornarDataDeOntem(string texto)
         {
             var dataDeOntem = DateTime.Now.AddDays(-1);
-
             Assert.Equal(dataDeOntem.Date, Data.Obter(texto).Date);
+        }
+
+        [Theory]
+        [InlineData("20/01/2020")]
+        [InlineData("20-01-2020")]
+        [InlineData("2020-01-20")]
+        public void Obter_DataSemHoraInformada_DeveRetornarDataInformada(string texto)
+        {
+            var dataEsperada = new DateTime(2020, 01, 20);
+            Assert.Equal(dataEsperada, Data.Obter(texto));
+        }
+
+        [Theory]
+        [InlineData("15/03/2020 12:59:59")]
+        [InlineData("15-03-2020 12:59:59")]
+        [InlineData("2020-03-15 12:59:59")]
+        public void Obter_DataComHoraInformada_DeveRetornarDataInformada(string texto)
+        {
+            var dataEsperada = new DateTime(2020, 03, 15, 12, 59, 59);
+            Assert.Equal(dataEsperada, Data.Obter(texto));
         }
     }
 }
